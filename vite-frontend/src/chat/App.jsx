086d64ebeba4d672fc6ed1c6485b523b489ac755
Chinta Sunil Varma <160120737114.sunilvarma@gmail.com>
2023-04-05 16:19:59 +0530

    fixed bug

--- vite-frontend/src/chat/App.jsx
+++ vite-frontend/src/chat/App.jsx
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("http://localhost:5000/");
+const socket = io.connect("https://club-gwnn.onrender.com");
 
 function AppC() {
   const [username, setUsername] = useState("");
